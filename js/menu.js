9b99f0fb2b7f78bc772760275641922464035332
Pro100Den4ik163 <Pro100Den4ik163@gmail.com>
2020-04-18 09:45:52 +0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    $('.navigation__burger').click(function(event){
+$(document).ready(function () {
+    $('.navigation__burger').click(function (event) {
         $('.navigation__burger,.navigation__list').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
